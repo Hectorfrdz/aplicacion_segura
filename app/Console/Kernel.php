cb18ef4531f669b85be8aa7c6ae5dff10bcebb19
Hectorfrdz <hectorflores_rdz@outlook.com>
2024-03-17 20:05:46 -0600

    schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Jobs\Verificacion_Dos_Pasos;
+use App\Jobs\Verificar_Correo;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,6 +15,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->job(Verificacion_Dos_Pasos::class)->everyMinute();
+        $schedule->job(Verificar_Correo::class)->everyMinute();
     }
 
     /**
